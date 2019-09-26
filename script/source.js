7cf19a4281cd9166d924ed2b90bba4776e274509
Alex Wishart <wishart.a470@gmail.com>
2019-09-26 12:53:02 +0100

    added basic storage with no implimented reset

--- script/source.js
+++ script/source.js
@@ -1,19 +1,36 @@
 window.addEventListener("load", init);
+
 function init(){
+	var heads_out = document.querySelector("#heads_out");
+	var tails_out = document.querySelector("#tails_out");
 	document.querySelector("#heads_butt").addEventListener("click", heads);
 	document.querySelector("#tails_butt").addEventListener("click", tails);
+	defaultCounts();
+}
+
+function store(){
+	var heads_store = parseInt(heads_out.textContent, 10);
+	var tails_store = parseInt(tails_out.textContent, 10);
+	localStorage.setItem('headsStore', heads_store);
+	localStorage.setItem('tailsStore', tails_store);
+}
+
+function defaultCounts(){
+	var heads_load = localStorage.getItem('headsStore');
+	var tails_load = localStorage.getItem('tailsStore');
+	heads_out.textContent = heads_load;
+	tails_out.textContent = tails_load;
 }
 
 function heads(){
-	var heads_out = document.querySelector("#heads_out");
 	var heads_count = parseInt(heads_out.textContent, 10);
 	heads_count++;
 	heads_out.textContent = heads_count;
-	
+	store();
 }
 function tails(){
-	var tails_out = document.querySelector("#tails_out");
 	var tails_count = parseInt(tails_out.textContent, 10);
 	tails_count++;
 	tails_out.textContent = tails_count;
+	store();
 }
