3793eb65ece1a437e758fc2e050db1236a61c835
Alex Wishart <wishart.a470@gmail.com>
2019-09-26 13:25:22 +0100

    added a total counter

--- script/source.js
+++ script/source.js
@@ -3,6 +3,7 @@ window.addEventListener("load", init);
 function init(){
 	var heads_out = document.querySelector("#heads_out");
 	var tails_out = document.querySelector("#tails_out");
+	var total = document.querySelector("#total");
 	document.querySelector("#heads_butt").addEventListener("click", heads);
 	document.querySelector("#tails_butt").addEventListener("click", tails);
 	document.querySelector("#reset").addEventListener("click", resetCounts);
@@ -12,32 +13,44 @@ function init(){
 function store(){
 	var heads_store = parseInt(heads_out.textContent, 10);
 	var tails_store = parseInt(tails_out.textContent, 10);
+	var total_store = parseInt(total.textContent, 10);
 	localStorage.setItem('headsStore', heads_store);
 	localStorage.setItem('tailsStore', tails_store);
+	localStorage.setItem('totalStore', total_store);
+	loadCounts();
 }
 
 function loadCounts(){
 	var heads_load = localStorage.getItem('headsStore');
 	var tails_load = localStorage.getItem('tailsStore');
+	var total_load = localStorage.getItem('totalStore');
 	heads_out.textContent = heads_load;
 	tails_out.textContent = tails_load;
+	total.textContent = total_load;
 }
 
 function heads(){
 	var heads_count = parseInt(heads_out.textContent, 10);
+	var total_count = parseInt(total.textContent, 10);
 	heads_count++;
 	heads_out.textContent = heads_count;
+	total_count++;
+	total.textContent  = total_count;
 	store();
 }
 function tails(){
 	var tails_count = parseInt(tails_out.textContent, 10);
+	var total_count = parseInt(total.textContent, 10);
 	tails_count++;
 	tails_out.textContent = tails_count;
+	total_count++;
+	total.textContent  = total_count;
 	store();
 }
 
 function resetCounts(){
 	localStorage.setItem('headsStore', 0);
 	localStorage.setItem('tailsStore', 0);
+	localStorage.setItem('totalStore', 0);
 	loadCounts();
 }
